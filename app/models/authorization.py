fb4e03e562ab85319c8e98641467ae50fcf83d4b
suman phuyal <sfuyal59@gmail.com>
2026-06-19 17:38:57 +0545

    Add orm relationship

--- app/models/authorization.py
+++ app/models/authorization.py
@@ -1,26 +1,35 @@
 import uuid
+from typing import TYPE_CHECKING
+
 from sqlalchemy import String
 from sqlalchemy import UniqueConstraint, UUID, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from app.db.database import Base
 
+if TYPE_CHECKING:
+    from app.models.user import User
 
 class Role(Base):
     __tablename__ = "roles"
 
     id: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     name: Mapped[str] = mapped_column(String(50), unique=True, nullable=False)
+    
+    users: Mapped[list["User"]] = relationship("User", back_populates="role")
+    permissions: Mapped[list["Permission"]] = relationship("Permission", secondary="role_permissions", back_populates="roles")
 
 
 class Permission(Base):
     __tablename__ = "permissions"
-    id: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True), primary_key=True)
+    id: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     action: Mapped[str] = mapped_column(String(50))
     resource: Mapped[str] = mapped_column(String(50))
 
     __table_args__ = (
         UniqueConstraint('action', 'resource', name='uc_action_resource'),
     )
+    roles: Mapped[list["Role"]] = relationship("Role", secondary="role_permissions", back_populates="permissions")
+
 
 class RolePermission(Base):
     __tablename__ = "role_permissions"
@@ -35,5 +44,7 @@ class RolePermission(Base):
         ForeignKey("permissions.id", ondelete="CASCADE"),
         primary_key=True
     )
+
+
 
 
